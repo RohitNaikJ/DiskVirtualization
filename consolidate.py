7d1a23cb81966c0c466c9f52ee2698f734b56d67
Akram1312 <mdd.akram@gmail.com>
2017-09-16 22:44:43 +0530

    Update consolidate.py
    number of disks are dynamic now

--- consolidate.py
+++ consolidate.py
@@ -8,25 +8,47 @@ class diskV:
 
     def write(self, no, data):
         self.lst[no] = data
+        
+    def size(self):
+        return len(self.lst)
 
 # Main class
 class consolidate():
-    def __init__(self):
-        self.disk1 = diskV(200)
-        self.disk2 = diskV(300)
+    def __init__(self, hdisks):
+        self.disks = []
+        for i in hdisks:
+            self.disks.append(diskV(i))
+        # self.disk1 = diskV(200)
+        # self.disk2 = diskV(300)
 
     def readDisk(self,blockNo):
-        if blockNo < 200:
-            return self.disk1.read(blockNo)
-        else:
-            return self.disk2.read(blockNo-200)
+        total = 0
+        prev = diskV(0)
+        for i in self.disks:
+            total += i.size()
+            if(blockNo < total):
+                return i.read(blockNo-(total-prev.size()))
+            prev = i
+        print("out of bounds")
+        return
+        # if blockNo < 200:
+        #     return self.disk1.read(blockNo)
+        # else:
+        #     return self.disk2.read(blockNo-200)
 
     def writeDisk(self, blockNo, info):
-        if blockNo < 200:
-            self.disk1.write(blockNo, info)
-        else:
-            self.disk2.write(blockNo-200, info)
+        total = 0
+        prev = diskV(0)
+        for i in self.disks:
+            total += i.size()
+            if(blockNo < total):
+                return i.write(blockNo-(total-prev.size()),info)
+            prev = i
+        # if blockNo < 200:
+        #     self.disk1.write(blockNo, info)
+        # else:
+        #     self.disk2.write(blockNo-200, info)
 
-cns = consolidate()
-cns.writeDisk(430, "Hello")
-print(type(cns.readDisk(430)))
+cns = consolidate([200,300,500])
+cns.writeDisk(730, "Hello")
+print(type(cns.readDisk(730)))
